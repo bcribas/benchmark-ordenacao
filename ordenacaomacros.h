ca5842e83a1ed2014b08379daa17e2fbdead96d1
Bruno Ribas <brunoribas@gmail.com>
2020-10-06 13:18:37 -0300

    ordenacaomacros.h: Criado com as macros padrões para inteiros
    Signed-off-by: Bruno Ribas <brunoribas@gmail.com>

---
+++ ordenacaomacros.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright(C) 2020, Bruno César Ribas <bruno.ribas@unb.br>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2.1 of the GNU Lesser General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it would be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+#ifndef __ORDENACAOMACROS__
+#define __ORDENACAOMACROS__
+typedef int Item;
+#define less(A,B) ((A) < (B))
+#define lesseq(A,B) ((A) <= (B))
+#define exch(A,B) { int t; t=A;A=B;B=t; }
+#define cmpexch(A,B) { if (less(B,A)) exch(A,B); }
+
+#endif
