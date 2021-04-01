1d7631a49a5502828cd6a517fbfcc7ac24f7ab2d
Bruno Ribas <brunoribas@gmail.com>
2021-04-01 15:20:33 -0300

    ordenacaomacros.h: Arruma tipo para a macro exch
    Signed-off-by: Bruno Ribas <brunoribas@gmail.com>

--- ordenacaomacros.h
+++ ordenacaomacros.h
@@ -15,7 +15,7 @@
 typedef int Item;
 #define less(A,B) ((A) < (B))
 #define lesseq(A,B) ((A) <= (B))
-#define exch(A,B) { int t; t=A;A=B;B=t; }
+#define exch(A,B) { Item t; t=A;A=B;B=t; }
 #define cmpexch(A,B) { if (less(B,A)) exch(A,B); }
 
 #endif
