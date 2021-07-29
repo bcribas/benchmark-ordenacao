9804b3a7660820f2e63d1977ab035bf0413a19b6
Christian Fleury <chfleurysiq@gmail.com>
2021-07-29 19:12:47 -0300

    ajuste bubblesortsentinela

--- bubblesortsentinela.c
+++ bubblesortsentinela.c
@@ -1,40 +1,25 @@
-#include <stdio.h>
-// stable and adaptive
-// O(n^2) 
-void swap(int *x, int *y) {
-  int t = *x;
-  *x = *y;
-  *y = t;
-}
+#include "ordenacaomacros.h"
 
-void bubbleSort(int A[], int n) {
-  int flag;
+bubbleSort(Item *v, int l, int r)
+{
+  int sentinela;
 
-  for (int i = 0; i<n-1; i++) {
-    flag = 0;
+  for (int i = l; i < r; i++)
+  {
+    sentinela = 0;
 
-    for(int j = 0; j<n-1-i; j++) {
-      if (A[j] > A[j+1]){
+    for (int j = l + 1; j <= r; j++)
+    {
+      if (less(v[j], v[j - 1]))
+      {
 
-        swap(&A[j], &A[j+1]);
-        flag = 1;
+        exch(v[j - 1], v[j]);
+        sentinela = 1;
       }
     }
-    // if it didn't make any swap the array is already sorted so it breaks the for loop (i)
-    if (!flag) {
+    if (!sentinela)
+    {
       break;
     }
   }
-}
-
-int main () {
-  int A[] = {2, 1, 8, 3, 4, 7, 6, 5};
-
-  bubbleSort(A, 8);
-
-  for (int i = 0; i<8; i++) {
-    printf("%d ", A[i]);
-  }
-
-  return 0;
 }
