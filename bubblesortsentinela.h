34eee25c6a7cf68a9758b37d31d18bd789eaa9e8
Christian Fleury <chfleurysiq@gmail.com>
2021-07-30 16:27:32 -0300

    bubblesortSentinela adicionado

--- bubblesortsentinela.h
+++ bubblesortsentinela.h
@@ -11,23 +11,13 @@
  *
  */
 
+#ifndef __bubblesortSentinelaH__
+#define __bubblesortSentinelaH__
 #include "ordenacaomacros.h"
+void bubblesortSentinela(Item *, int, int);
 
-void bubblesortSentinela(Item *v, int l, int r)
-{
-  int sentinela;
-  for (int i = l; i < r; i++)
-  {
-    sentinela = 0;
-    for (int j = l + 1; j <= r; j++)
-    {
-      if (less(v[j], v[j - 1]))
-      {
-        exch(v[j - 1], v[j]);
-        sentinela = 1;
-      }
-    }
-    if (!sentinela)
-      break;
-  }
-}
+#ifdef __bubblesortSentinelaonly__
+#define sort(v, l, r) bubblesortSentinela(v, l, r)
+#endif
+
+#endif
